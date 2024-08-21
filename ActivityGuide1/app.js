5cc575af6b55fe0c5e78a9913eb77d3f19a58ac3
veran_r <veran_r@f112.lan>
2024-08-21 12:31:58 +0800

    I am uploading the documents for my first activity in JavaScript

---
+++ ActivityGuide1/app.js
@@ -0,0 +1,16 @@
+//This is the component for the first item
+const btn1 = document.getElementById("btn1")
+//for displaying the information
+btn1.addEventListener("click", () => {
+    //create and initialize our variable
+    let name = document.getElementById("name").value
+    let address = document.getElementById("address").value
+    let telephone = document.getElementById("telephone").value
+    let major = document.getElementById("major").value
+
+    //display message in a template String
+    let x = `Hello! ${name}, I see that you are from ${address}
+    and you are also pursuing ${major} and you can be contacted
+    using ${telephone}.`
+    document.getElementById("output1").innerHTML= x
+})

f0b85fdcb454d1515a373eb81f1ed7fa999777f7
veran_r <veran_r@f112.lan>
2024-09-11 12:26:19 +0800

    Update of files

--- react-profile/src/Body.jsx
+++ react-profile/src/Body.jsx
@@ -7,7 +7,7 @@ export default function Body(){
                 </div>
                 <h1>Rens Zedric R. Veran</h1>
                 <p>Hellowww, My name is Rens Zedric Veran and I am a 2nd year student BSCS in UB. I mainly enjoy watching anime and movies and I usually play games on my free time.
-                    I chose this course because coding is interesting and I like doing stuff in the computer!.
+                    I chose this course because coding is interesting and I like doing stuff on the computer!.
                 </p>
             </div>
         </div>

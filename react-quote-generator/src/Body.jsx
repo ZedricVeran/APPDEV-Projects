c1f66d8cac94a50b32db9eca0bdf36f0d80b071e
veran_r <veran_r@f112.lan>
2024-09-27 13:42:32 +0800

    Update

---
+++ react-quote-generator/src/Body.jsx
@@ -0,0 +1,19 @@
+export default function Body() {
+  return (
+    <body>
+      <main>
+        <div class="container">
+          <h2>
+            Plato:
+            <span class="quote">
+              "A good decision is based on knowledge and not on numbers."
+            </span>
+          </h2>
+          <button class="btn btn-hero" id="btn">
+            Clicke me!
+          </button>
+        </div>
+      </main>
+    </body>
+  );
+}
---
+++ react-quote-generator/src/Header.jsx
@@ -0,0 +1,13 @@
+//This is my header component
+export default function Header(){
+    return(
+        <header>
+        <nav>
+        <div class="nav-center">
+            <h4>Quote Generator</h4>
+        </div>
+
+    </nav>
+        </header>
+    )
+}

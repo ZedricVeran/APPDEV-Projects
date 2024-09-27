f1d3cd8b0f0442d3ab8a3d65178f235c00466206
veran_r <veran_r@f112.lan>
2024-09-27 13:45:59 +0800

    Update

--- react-quote-generator/src/Body.jsx
+++ react-quote-generator/src/Body.jsx
@@ -1,5 +1,31 @@
 export default function Body() {
-  return (
+    const randomQuote = [
+        "\"A good decision is based on knowledge and not on numbers.\"",
+        "\"Thinking - the talking of the soul with itself.\"",
+        "\"There is no harm in repeating a good thing.\"",
+        "\"Truth is the beginning of every good to the gods, and of every good to man.\"",
+        "\"Knowledge without justice ought to be\""
+    ]
+    
+    
+    
+    const [quote,setQuote] = useState("\"A good decision is based on knowledge and not on numbers.\"")
+    
+    function getRandomNumber() {
+        return Math.floor(Math.random()*randomColor.length)
+
+    }
+
+    function getRandomQuote(){
+        let quotes=randomQuote[getRandomNumber()] 
+    }
+
+    function randomQuoteChange(){
+        
+    }
+  
+  
+    return (
     <body>
       <main>
         <div class="container">
@@ -9,9 +35,8 @@ export default function Body() {
               "A good decision is based on knowledge and not on numbers."
             </span>
           </h2>
-          <button class="btn btn-hero" id="btn">
-            Clicke me!
-          </button>
+          <button class="btn btn-hero" id="btn">Randomize</button>
+          <button class="btn btn-hero" id="btn">Order</button>
         </div>
       </main>
     </body>

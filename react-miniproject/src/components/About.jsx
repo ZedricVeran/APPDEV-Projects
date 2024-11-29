15f012488acb6109300a388a2e986d4400159f61
veran_r <veran_r@f112.lan>
2024-11-29 13:36:07 +0800

    mini project update2

--- react-miniproject/src/components/About.jsx
+++ react-miniproject/src/components/About.jsx
@@ -1,7 +1,10 @@
 export default function About(){
     return(
         <>
-            <h1>Here is the about page!</h1>
+            <h1>Mission</h1>
+            <p>To craft high-quality, functional, and visually stunning keyboards that enhance the typing experience for everyone—from professionals to PC enthusiasts—while fostering creativity and individuality.</p>
+            <h1>Vision</h1>
+            <p>To become the leading brand in personalized and artisan keyboards, inspiring a global community to elevate their work and gaming setups with our innovative designs.</p>
         </>
     )
 }

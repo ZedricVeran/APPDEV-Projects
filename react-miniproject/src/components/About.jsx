780794e2694165c58cf66357ef2832c0354b2208
veran_r <veran_r@f112.lan>
2024-12-06 13:45:09 +0800

    Mini project update 3

--- react-miniproject/src/components/About.jsx
+++ react-miniproject/src/components/About.jsx
@@ -1,10 +1,38 @@
 export default function About(){
     return(
         <>
-            <h1>Mission</h1>
-            <p>To craft high-quality, functional, and visually stunning keyboards that enhance the typing experience for everyone—from professionals to PC enthusiasts—while fostering creativity and individuality.</p>
-            <h1>Vision</h1>
-            <p>To become the leading brand in personalized and artisan keyboards, inspiring a global community to elevate their work and gaming setups with our innovative designs.</p>
+            <div className="mission-section">
+                <img src="" alt="" />
+                <h1>Mission</h1>
+                <p>To craft high-quality, functional, and visually stunning keyboards that enhance the typing experience for everyone—from professionals to PC enthusiasts—while fostering creativity and individuality.</p>
+            </div>
+            <div className="vision-seciton">
+                <img src="" alt="" />
+                <h1>Vision</h1>
+                <p>To become the leading brand in personalized and artisan keyboards, inspiring a global community to elevate their work and gaming setups with our innovative designs.</p>
+            </div>
+
+            <div className="employee-section">
+                <h1>Meet the team</h1>
+                <div className="employee1">
+                    <img src="" alt="" />
+                    <h1>Employee1</h1>
+                    <h4>Job</h4>
+                    <button type="button">Check CV</button>
+                </div>
+                <div className="employee2">
+                    <img src="" alt="" />
+                    <h1>Employee2</h1>
+                    <h4>Job</h4>
+                    <button type="button">Check CV</button>
+                </div>
+                <div className="employee3">
+                    <img src="" alt="" />
+                    <h1>Employee3</h1>
+                    <h4>Job</h4>
+                    <button type="button">Check CV</button>
+                </div>
+            </div>
         </>
     )
 }
--- react-miniproject/src/components/Developer.jsx
+++ react-miniproject/src/components/Developer.jsx
@@ -1,7 +1,57 @@
 export default function Developer(){
     return(
         <>
-            <h1>Here is the Devs page!</h1>
+        <h1>Meet the developer!</h1>
+        <div className="dev-banner1">
+            <div className="section1">
+                <img src="https://via.placeholder.com/650x650" alt="Profile-pic" />
+                <h1>Rens Zedric Veran</h1>
+                <p>Developer</p>
+            </div>
+            <div className="section2">
+
+            </div>
+        </div>
+        <div className="dev-banner2">
+            <div className="about-me-section">
+                <h1>About Me</h1>
+                <hr />
+                <p>I am an aspiring software engineer currently studying at the University of Baguio, where I am building the foundation for a career in technology. Passionate about problem-solving and innovation, I am constantly exploring new programming languages, tools, and frameworks to enhance my skills. Through both my academic work and personal projects, I strive to develop efficient, user-friendly solutions that make a positive impact. Eager to learn and grow in this ever-evolving field, I look forward to contributing to the world of software engineering and continuously pushing the boundaries of what’s possible.</p>
+            </div>
+            <div className="doing-section">
+                <div className="doing1">
+                    <img src="https://via.placeholder.com/120x120" alt="" />
+                    <h4>Website development</h4>
+                    <p>High quality development of websites</p>
+                </div>
+                <div className="doing2">
+                    <img src="https://via.placeholder.com/120x120" alt="" />
+                    <h4>Website desing</h4>
+                    <p>Web design up to trends</p>
+                </div>
+                <div className="doing3">
+                    <img src="https://via.placeholder.com/120x120" alt="" />
+                    <h4>Video Editing</h4>
+                    <p>Edit videos in a professional level</p>
+                </div>
+                <div className="doing4">
+                    <img src="https://via.placeholder.com/120x120" alt="" />
+                    <h4>Photo Editing</h4>
+                    <p>Photo manipulation for your webisites</p>
+                </div>
+            </div>
+            <div className="skills-section">
+                <ul>
+                    <li>Java</li>
+                    <li>ReactJS</li>
+                    <li>JS</li>
+                    <li>Python</li>
+                    <li>HTML/CSS</li>
+                </ul>
+            </div>
+        </div>
+            
+
         </>
     )
 }
